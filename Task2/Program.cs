60a8f3fc69ae05733c2d4a19441d5ca77eb2a6be
Daria <darakirichenko@gmail.com>
2023-02-13 00:21:33 +0300

    Task 2 is done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,3 +7,13 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число:");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
+if(number2 > max)
+{
+    max = number2;
+}
+if(number3 > max)
+{
+    max = number3;
+}
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);
